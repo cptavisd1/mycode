e65212ec26f857a6fa8578986ac3bdbd2542a296
Avis Daniels <avis@danielsltd.onmicrosoft.com>
2019-07-22 13:33:56 -0400

    testing with if

---
+++ if-test2/iftest2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+ipchk = input('Apply an ip addresss: ')
+
+if ipchk:
+    print('looks like the ip address was set: ' + ipchk)
+else:
+    print('You did not provide input. ')
+
